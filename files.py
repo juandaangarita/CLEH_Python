cf90fd0e445a5fa2eff10e6470637e9ffa79ffa9
Juan David Angarita <juandaangarita@gmail.com>
2021-10-29 14:32:12 -0500

    There was created a program to know how to handle files

---
+++ files.py
@@ -0,0 +1,23 @@
+def read():
+    numbers = []
+    with open("./files/numbers.txt", "r", encoding="utf-8") as f:
+        for line in f:
+            numbers.append(int(line))
+    print (numbers)
+
+
+def write():
+    names = ["Facundo Nicolas", "Daniel", "Juan", "Sebastián"]
+    with open("./files/name.txt", "a", encoding="utf-8") as f:
+        for name in names:
+            f.write(name)
+            f.write("\n")
+
+
+def run():
+    read()
+    write()
+
+
+if __name__ == '__main__':
+    run()
